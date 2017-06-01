561a2ee3772363b6ced5ee4dd9d1ada64c5aef43
Samuel Manoel Silveira <samuelsilveiram@gmail.com>
2017-06-01 06:39:55 -0300

    Iniciando com tensor flow

--- machine-learning/classificacao.py
+++ machine-learning/classificacao.py
@@ -16,8 +16,24 @@ marcacoes = [1, 1, 1, -1, -1, -1]
 modelo = MultinomialNB()
 modelo.fit(dados, marcacoes)
 
-misterioso = [1, 1, 1]
-misterioso2 = [1, 1, 0]
+misterioso =  [1, 1, 1]
+misterioso2 = [1, 0, 0]
+misterioso3 = [0, 0, 1]
 
-teste = [misterioso, misterioso2]
-print(modelo.predict(teste))
+testes = [misterioso, misterioso2,misterioso3]
+
+marcacoes_teste = [-1,1,-1];
+
+resultado = modelo.predict(testes)
+
+print(resultado)
+
+diferencas = resultado - marcacoes_teste
+acertos = [d for d in diferencas if d == 0]
+
+print(acertos)
+
+total_de_acertos = len(acertos)
+total_de_elementos = len (testes)
+taxa_de_acerto = total_de_acertos / total_de_elementos * 100;
+print(taxa_de_acerto)
