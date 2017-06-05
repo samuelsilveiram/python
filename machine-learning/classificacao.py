096b3a39a08c2a955484a858b78332988258a46a
Samuel Silveira <samuel.silveira@involves.com.br>
2017-06-05 12:48:10 -0300

    Finalizando implementação

--- machine-learning/classificacao.py
+++ machine-learning/classificacao.py
@@ -1,23 +1,40 @@
 # eh gordinho? tem perninha curta? se faz au-au
 
-porco1 =    [1, 1, 0]
-porco2 =    [1, 1, 0]
-porco3 =    [1, 1, 0]
-cachorro1 = [1, 1, 1]
-cachorro2 = [0, 1, 1]
-cachorro3 = [0, 1, 1] 
+porco1 = [1, 1, 0]
+porco2 = [1, 1, 0]
+porco3 = [1, 1, 0]
+cachorro4 = [1, 1, 1]
+cachorro5 = [0, 1, 1]
+cachorro6 = [0, 1, 1]
 
-dados = [porco1, porco2, porco3, cachorro1, cachorro2, cachorro3]
-
-from sklearn.naive_bayes import MultinomialNB
+dados = [porco1, porco2, porco3, cachorro4, cachorro5, cachorro6]
 
 marcacoes = [1, 1, 1, -1, -1, -1]
+
+from sklearn.naive_bayes import MultinomialNB
 
 modelo = MultinomialNB()
 modelo.fit(dados, marcacoes)
 
-misterioso = [1, 1, 1]
-misterioso2 = [1, 1, 0]
+misterioso1 = [1, 1, 1]
+misterioso2 = [1, 0, 0]
+misterioso3 = [0, 0, 1]
 
-teste = [misterioso, misterioso2]
-print(modelo.predict(teste))
+teste = [misterioso1, misterioso2, misterioso3]
+
+marcacoes_teste = [-1, 1, 1]
+
+resultado = modelo.predict(teste)
+
+diferencas = resultado - marcacoes_teste
+
+acertos = [d for d in diferencas if d == 0]
+
+total_de_acertos = len(acertos)
+total_de_elementos = len(teste)
+
+taxa_de_acerto = 100.0 * total_de_acertos / total_de_elementos
+
+print(resultado)
+print(diferencas)
+print(taxa_de_acerto)
