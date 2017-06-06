c3ad58b2e6322671a8f41752e6c5ec65d829f30e
Samuel Manoel Silveira <samuelsilveiram@gmail.com>
2017-06-05 23:05:48 -0300

    Finaliando exercicio

--- machine-learning/acessos/classifica_acessos.py
+++ machine-learning/acessos/classifica_acessos.py
@@ -2,5 +2,27 @@ from dados import carregar_acessos
 
 x, y = carregar_acessos()
 
-print(x)
-print(y)
+treino_dados = x[:90]
+treino_marcacoes = y[:90]
+
+teste_dados = x[-9:]
+teste_marcacoes = y[-9:]
+
+from sklearn.naive_bayes import MultinomialNB
+
+modelo = MultinomialNB()
+modelo.fit(treino_dados,treino_marcacoes)
+
+resultado = modelo.predict(teste_dados)
+
+diferencas = resultado - teste_marcacoes
+
+acertos = [d for d in diferencas if d == 0]
+
+total_acertos = len(acertos)
+total_elementos = len(teste_dados)
+
+taxa_acerto = 100.0 * total_acertos / total_elementos
+
+print(taxa_acerto)
+print(total_elementos)
